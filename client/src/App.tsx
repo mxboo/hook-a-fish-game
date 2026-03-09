53a1046533399dfe4c054c76f7bb8cafed7a6c71
Bomi <abee@example.com>
2026-03-10 04:14:58 +0800

    refactor: 完全移除 Leva 调试面板代码
    - 删除 Leva 调试工具的注释代码
    - 清理 App.tsx 中的调试相关代码
    - 保持代码简洁

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,7 @@ import Experience from './components/Experience'
 export default function App() {
   return (
     <>
-      {/* Leva 调试面板已隐藏 */}
-      {/* <Leva hidden={!debug} collapsed theme={{ sizes: { rootWidth: '350px' } }} /> */}
-
-      {/* See https://github.com/pmndrs/leva/issues/552 */}
+      {/* 调试面板已移除 */}
       <StrictMode>
         <Experience />
       </StrictMode>
