e63444e607b213afd923942116b28adea3fadafe
Bomi <abee@example.com>
2026-03-10 04:12:34 +0800

    feat: 隐藏右上角 Leva 调试面板
    - 移除开发环境调试工具 Leva 的显示
    - 清理游戏界面，提供更干净的游戏体验
    - 如需要调试可通过代码取消注释

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import { Leva } from 'leva'
 import { StrictMode } from 'react'
 import Experience from './components/Experience'
-import { useDebug } from './hooks/use-debug'
 
 export default function App() {
-  const debug = useDebug()
-
   return (
     <>
-      <Leva hidden={!debug} collapsed theme={{ sizes: { rootWidth: '350px' } }} />
+      {/* Leva 调试面板已隐藏 */}
+      {/* <Leva hidden={!debug} collapsed theme={{ sizes: { rootWidth: '350px' } }} /> */}
 
       {/* See https://github.com/pmndrs/leva/issues/552 */}
       <StrictMode>
